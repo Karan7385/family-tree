0bbf0a9427dc6c595fddfa3bfae67ec608b8a9bc
Karan Vishwakarma <132004206+Karan7385@users.noreply.github.com>
2026-03-24 00:15:08 +0530

    Set secure cookie flag to true unconditionally

--- backend/controllers/authentication/authController.js
+++ backend/controllers/authentication/authController.js
@@ -45,7 +45,7 @@ const register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
@@ -101,7 +101,7 @@ const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
