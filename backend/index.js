88b1957994f6eaed2ec428ab52d182e47db952a4
Karan Vishwakarma <132004206+Karan7385@users.noreply.github.com>
2026-03-23 23:11:14 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://family-tree-seven-flax.vercel.app/",
   credentials: true
 }));
 app.use(cookieParser());
