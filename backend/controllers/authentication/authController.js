5b6dfd830f90993f19d95c2182076651c8d570ad
Karan Vishwakarma <132004206+Karan7385@users.noreply.github.com>
2026-03-24 01:01:53 +0530

    Set httpOnly to true for authentication cookies
    Updated cookie settings to enhance security by setting httpOnly to true.

--- backend/controllers/authentication/authController.js
+++ backend/controllers/authentication/authController.js
@@ -44,7 +44,7 @@ const register = async (req, res) => {
     });
 
     res.cookie("token", token, {
-      httpOnly: false,
+      httpOnly: true,
       secure: true,
       sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -100,7 +100,7 @@ const login = async (req, res) => {
     });
 
     res.cookie("token", token, {
-      httpOnly: false,
+      httpOnly: true,
       secure: true,
       sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000,
