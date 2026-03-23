477d9f98d542990b1d7aa8c009edfcd9b7ea5c20
Karan Vishwakarma <132004206+Karan7385@users.noreply.github.com>
2026-03-23 23:45:38 +0530

    Restrict CORS to specific origin

--- backend/index.js
+++ backend/index.js
@@ -22,9 +22,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: '*',
-  methods: '*',
-  allowedHeaders:
+  origin: 'https://family-tree-seven-flax.vercel.app',
   credentials: true
 }));
 app.use(cookieParser());
